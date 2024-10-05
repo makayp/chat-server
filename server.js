474da369fa26c28d1db9942808c9301986d70e87
Emmanuel Popoola <popoolae1@gmail.com>
2024-10-04 20:43:43 -0500

    Clean up code

--- server.js
+++ server.js
@@ -2,59 +2,16 @@ import express from 'express';
 import { Server } from 'socket.io';
 import { configDotenv } from 'dotenv';
 
-configDotenv();
 const app = express();
-let messages = [
-  {
-    id: 1,
-    sender: 'Emmanuel',
-    message: 'Hello',
-    time: '2024-06-14T14:53:10.665Z',
-  },
-  {
-    id: 2,
-    sender: 'David',
-    message: 'Hi',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-  {
-    id: 3,
-    sender: 'Emmanuel',
-    message: 'How are you all doing today?',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-  {
-    id: 4,
-    sender: 'Em',
-    message: 'Hi',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-  {
-    id: 5,
-    sender: 'David',
-    message: 'Hi',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-  {
-    id: 6,
-    sender: 'Emmanuel',
-    message:
-      'I am doing great, thanks, how are you doing too? and how is the weather over there?',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-  {
-    id: 7,
-    sender: 'Em',
-    message:
-      'I am doing great, thanks, how are you doing too? and how is the weather over there?',
-    time: '2024-06-14T15:02:55.504Z',
-  },
-];
 
-const PORT = 3000;
+configDotenv();
+
+let messages = [];
+
+const PORT = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Server is live!');
+  res.send(`Server is live! ${new Date().toLocaleString('en')}`);
 });
 
 const expressServer = app.listen(PORT, () => {
@@ -63,11 +20,10 @@ const expressServer = app.listen(PORT, () => {
 
 const io = new Server(expressServer, {
   cors: {
-    origin: [
-      'https://chat-app-mkp.netlify.app',
-      'https://demochat.emmanuelp.dev',
-      'http://localhost:5173',
-    ],
+    origin: ['https://demochat.emmanuelp.dev', 'http://localhost:5173'],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
   },
 });
 
@@ -105,7 +61,7 @@ function clearOldMessages() {
     return currentTime - messageTime < ONE_HOUR;
   });
 
-  io.emit('welcome', messages);
+  io.to('public chat').emit('clear messages', messages);
 }
 
 setInterval(() => {
